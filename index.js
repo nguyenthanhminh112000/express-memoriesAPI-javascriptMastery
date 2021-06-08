0631eddd8317136ae1ce3a88dfbc4eb814143d3a
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-08 23:23:46 +0700

    Connect to mongodb, create PostMessage Model, set-up routers,routes,controllers,models

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import postsRouter from './routers/postsRouter.js';
+
+/////////////////CREATE EXPRESS-APP
+const app = express();
+///////////////// USE MIDDLEWARE
+//parse incoming request with json
+app.use(express.json());
+//parse incoming request with urlencoded payloads
+app.use(express.urlencoded());
+// enable cors
+app.use(cors());
+
+/////////////////SET ROUTES
+app.use('/api/v1/posts', postsRouter);
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+//////////////// CONNECT TO DATABASE
+const CONNECTION_URL =
+  'mongodb+srv://memories:memories123@cluster0.h9mgz.mongodb.net/memoriesSocialNetwork?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+
+// STOP SHOW WARNING ABOUT DATABASE
+mongoose.set('useFindAndModify', false);
