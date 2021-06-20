243f776802bd90ff42659c9abdb06e7053c62db0
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-20 16:47:27 +0700

    Set-up environmental variable

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postsRouter from './routers/postsRouter.js';
+import dotenv from 'dotenv';
 
 /////////////////CREATE EXPRESS-APP
 const app = express();
+dotenv.config();
 ///////////////// USE MIDDLEWARE
 //parse incoming request with json
 app.use(express.json());
@@ -17,10 +19,9 @@ app.use(cors());
 app.use('/api/v1/posts', postsRouter);
 app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
 //////////////// CONNECT TO DATABASE
-const CONNECTION_URL =
-  'mongodb+srv://memories:memories123@cluster0.h9mgz.mongodb.net/memoriesSocialNetwork?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,
